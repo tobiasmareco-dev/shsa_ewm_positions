18238d80c8b59fa9aca7ce1b002083d7104b3c73
Tobias Mareco <tmareco@laboratorioscatedral.com.py>
2024-08-29 16:51:37 -0400

    change axios ur;

--- src/config/axiosConfig.jsx
+++ src/config/axiosConfig.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: "http://192.10.10.182:5555"
+    // baseURL: "http://192.10.10.182:5555"
+    baseURL: "http://catedral.scavonehnos.com.py:5555"
 });
 
 export default axiosClient;
